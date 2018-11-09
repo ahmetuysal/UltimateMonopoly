323a874ef4741045e4b2f69949371bf6caf1d1ec
Halil Eralp Koçaş <hkocas15@ku.edu.tr>
2018-11-09 21:51:03 +0300

    Factory pattern for cards
    Factory pattern for card classes is completed!

--- UltimateMonopoly/src/domain/card/CommunityChestCard.java
+++ UltimateMonopoly/src/domain/card/CommunityChestCard.java
@@ -5,38 +5,7 @@ public abstract class CommunityChestCard extends Card {
 
 	protected CommunityChestCard(String n, String d) {
 		super(n, d);
-		// TODO Auto-generated constructor stub
-		
+		// TODO Auto-generated constructor stub	
 	}
 	
-	public static CommunityChestCard getCard(String n, String d) {
-		if(n.equals(null)) return null;
-		
-		if("Happy Birthday!".equals(n)) {
-			return new HappyBirthday(n, d);
-		}else if("Game Night!".equals(n)) {
-			return new GameNight(n, d);
-		}else if("A Moving Experience".equals(n)) {
-			return new AMovingExperience(n, d);
-		}else if("HOUSE CONDEMNED".equals(n)) {
-			return new HouseCondemned(n, d);
-		}else if("Elected District Attorney".equals(n)) {
-			return new ElectedDistrictAttorney(n, d);
-		}else if("Deal Buster".equals(n)) {
-			return new DealBuster(n, d);
-		}else if("Be Kind, Rewind".equals(n)) {
-			return new BeKindRewind(n, d);
-		}else if("Pay Hospital Bills".equals(n)) {
-			return new PayHospitalBills(n, d);
-		}else if("Tornado Hits!".equals(n)) {
-			return new TornadoHits(n, d);
-		}else if("Share in their Good Fortune".equals(n)) {
-			return new ShareInTheirGoodFortune(n, d);
-		}else if("The Insider's Edge".equals(n)) {
-			return new TheInsidersEdge(n, d);
-		}
-		
-		return null;
-		
-	}
 }
---
+++ UltimateMonopoly/src/domain/card/CommunityChestCardFactory.java
@@ -0,0 +1,46 @@
+package domain.card;
+
+public class CommunityChestCardFactory {
+
+private static CommunityChestCardFactory instance;
+	
+	private CommunityChestCardFactory() {}
+	
+	public static synchronized CommunityChestCardFactory getInstance() {
+		if(instance == null)
+			instance = new CommunityChestCardFactory();
+		return instance;
+	}
+	
+	public static CommunityChestCard getCard(String n, String d) {
+		if(n.equals(null)) return null;
+		
+		if("Happy Birthday!".equals(n)) {
+			return new HappyBirthday(n, d);
+		}else if("Game Night!".equals(n)) {
+			return new GameNight(n, d);
+		}else if("A Moving Experience".equals(n)) {
+			return new AMovingExperience(n, d);
+		}else if("HOUSE CONDEMNED".equals(n)) {
+			return new HouseCondemned(n, d);
+		}else if("Elected District Attorney".equals(n)) {
+			return new ElectedDistrictAttorney(n, d);
+		}else if("Deal Buster".equals(n)) {
+			return new DealBuster(n, d);
+		}else if("Be Kind, Rewind".equals(n)) {
+			return new BeKindRewind(n, d);
+		}else if("Pay Hospital Bills".equals(n)) {
+			return new PayHospitalBills(n, d);
+		}else if("Tornado Hits!".equals(n)) {
+			return new TornadoHits(n, d);
+		}else if("Share in their Good Fortune".equals(n)) {
+			return new ShareInTheirGoodFortune(n, d);
+		}else if("The Insider's Edge".equals(n)) {
+			return new TheInsidersEdge(n, d);
+		}
+		
+		return null;
+		
+	}
+	
+}
