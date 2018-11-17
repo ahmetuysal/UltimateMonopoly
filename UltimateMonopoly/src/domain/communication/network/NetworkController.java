05267ca08541993f778d4bac4a2a8d4fd0f0fcdf
Meltem Gunay <mgunay15@ku.edu.tr>
2018-11-18 00:26:46 +0300

    Edited NetworkController

--- UltimateMonopoly/src/domain/communication/network/NetworkController.java
+++ UltimateMonopoly/src/domain/communication/network/NetworkController.java
@@ -1,15 +1,45 @@
 package domain.communication.network;
 
-import java.io.File;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 
 public class NetworkController {
 
+	static Connection connection = null; 
+	private final static ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 	private GameState currentGameState;
-	
+
 	public NetworkController(){
-		currentGameState = new GameState();
+		connectToGameServer();
+		periodicallyUpdateGameState();
 	}
 	
+	private static void connectToGameServer() {
+		connection = new Connection(Connection.DEFAULT_SERVER_ADDRESS, Connection.DEFAULT_SERVER_PORT);
+		connection.Connect();
+	}
+
+	/**
+	 * updates game state periodically
+	 */
+	public static void periodicallyUpdateGameState() {
+
+		final Runnable updatePeriodically = new Runnable() {
+			public void run() {
+				ClientHost ch = ClientHost.getInstance();
+				connection.updateGameState(ch.getPlayerGameState());
+				System.out.println("Client received:");
+				System.out.println(ch.getPlayerGameState());
+
+			}
+		};
+
+		final ScheduledFuture<?> checkHandle = scheduler.scheduleAtFixedRate(updatePeriodically, 1, 4, SECONDS);
+
+	}
+
 	public boolean isConnected() {
 		return true; //arbitrary
 	}
@@ -21,6 +51,6 @@ public class NetworkController {
 	public void setCurrentGameState(GameState currentGameState) {
 		this.currentGameState = currentGameState;
 	}
-	
-	
+
+
 }
