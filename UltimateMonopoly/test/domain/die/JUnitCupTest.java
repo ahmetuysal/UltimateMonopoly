828dc6c1280cd5cf825e0a1ebb994a75f5368a4d
Meltem Gunay <mgunay15@ku.edu.tr>
2018-12-13 00:39:29 +0300

    Black-box, glass-box tests for Cup

--- UltimateMonopoly/test/domain/die/JUnitCupTest.java
+++ UltimateMonopoly/test/domain/die/JUnitCupTest.java
@@ -1,6 +1,8 @@
 package domain.die;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +13,113 @@ import org.junit.jupiter.api.Test;
 
 class JUnitCupTest {
 
+	// Black-Box Testing
+	@Test
+	void testRollThreeRegularDices() {
+		Cup testCup = new Cup();
+		for(int i = 0 ; i < 100 ; i++) {
+			testCup.rollThreeRegularDices();
+			for(DieValue v : testCup.getFaceValues()) {
+				assertNotEquals(v,DieValue.MRMONOPOLY);
+				assertNotEquals(v,DieValue.BUSICON);
+				assertNotEquals(v,DieValue.EMPTY);
+			}
+		}
+	}
 	
+	@Test
+	void testGetTotal() {
+		Cup testCup = new Cup();
+		int sum;
+		for(int i = 0 ; i < 100 ; i++) {
+			testCup.rollDices();
+			sum = testCup.getTotal();
+			assertTrue(sum <= 18); 
+			assertTrue(sum >= 2);
+		}
+		
+		for(int i = 0 ; i < 100 ; i++) {
+			testCup.rollTwoRegularDices();
+			sum = testCup.getTotal();
+			assertTrue(sum <= 12); 
+			assertTrue(sum >= 2);
+		}
+	}
+	
+	@Test
+	void testClearCup() {
+		Cup testCup = new Cup();
+		for(int i = 0 ; i < 100 ; i++) {
+			testCup.rollDices();
+			testCup.clearCup();
+			for(DieValue v : testCup.getFaceValues()) {
+				assertEquals(v,DieValue.EMPTY);
+			}
+			
+		}
+		for(int i = 0 ; i < 100 ; i++) {
+			testCup.rollThreeRegularDices();
+			testCup.clearCup();
+			for(DieValue v : testCup.getFaceValues()) {
+				assertEquals(v,DieValue.EMPTY);
+			}
+			
+		}
+		for(int i = 0 ; i < 100 ; i++) {
+			testCup.rollTwoRegularDices();
+			testCup.clearCup();
+			for(DieValue v : testCup.getFaceValues()) {
+				assertEquals(v,DieValue.EMPTY);
+			}
+			
+		}
+	}
+	
+	
+	// End of Black-Box Testing
+	
+	
+	// Glass-Box Testing
+	
+	@Test
+	void testFaceValuesChanged() {
+		Cup testCup = new Cup();
+		int val1 = testCup.getFaceValues()[0].getValue();
+		int val2 = testCup.getFaceValues()[1].getValue();
+		int val3 = testCup.getFaceValues()[2].getValue();
+		boolean isChanged = false;
+		for(int i = 0 ; i < 100 ; i++) {
+			testCup.rollThreeRegularDices();
+			for(int j = 0 ; j < 3 ; j++) {
+				if(val1 != testCup.getFaceValues()[0].getValue() || val2 != testCup.getFaceValues()[1].getValue() || val3 != testCup.getFaceValues()[2].getValue()) {
+					isChanged = true;
+				}
+			}
+		}
+		assertTrue(isChanged);	
+	}
+	
+	
+	
+	
+	// End of Glass-Box Testing
+	
+	@Test
+	void repOKTestRollThreeRegularDices() {
+		Cup testCup = new Cup();
+		for(int i = 0 ; i < 100 ; i++) {
+			testCup.rollThreeRegularDices();
+			assertTrue(testCup.repOK());
+		}
+	}
 
+	@Test
+	void repOKTestClearCup() {
+		Cup testCup = new Cup();
+		for(int i = 0 ; i < 100 ; i++) {
+			testCup = new Cup();
+			testCup.clearCup();
+			assertTrue(testCup.repOK());
+		}
+	}
 }
