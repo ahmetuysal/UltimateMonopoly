3d2e06a84868aef9a73e85db9dacb2f9646d3677
Ahmet Uysal <auysal16@ku.edu.tr>
2018-12-16 23:34:16 +0300

    Fixed OwnableSquare StackOverFlow error. (Cyclic reference)

--- UltimateMonopoly/src/domain/square/OwnableSquare.java
+++ UltimateMonopoly/src/domain/square/OwnableSquare.java
@@ -4,33 +4,32 @@ import domain.GameController;
 import domain.Player;
 
 public abstract class OwnableSquare extends Square {
-	
+
 	protected int price;
 	protected Player owner;
 	protected boolean isOwned;
 
-	
 	public OwnableSquare(String name, String description, int price) {
 		super(name, description);
 		this.price = price;
 		this.owner = null;
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	/**
 	 * @return the price
 	 */
 	public int getPrice() {
 		return price;
 	}
-	
+
 	/**
 	 * @return the owner
 	 */
 	public Player getOwner() {
 		return owner;
 	}
-	
+
 	/**
 	 * @return the isOwned
 	 */
@@ -39,31 +38,31 @@ public abstract class OwnableSquare extends Square {
 	}
 
 	/**
-	 * @param owner the owner to set
+	 * @param owner
+	 *            the owner to set
 	 */
 	public void setOwner(Player owner) {
 		isOwned = owner != null;
 		this.owner = owner;
-		
+
 	}
-	
+
 	public boolean buyProperty(Player owner) {
-		if(isOwned) {
+		if (isOwned) {
 			System.out.println("Property " + this.getName() + " is already owned");
 			return false;
 		}
-		
-		if(owner.getTotalMoney() < this.price) {
+
+		if (owner.getTotalMoney() < this.price) {
 			System.out.println("Player " + owner.getNickName() + " does not have enough money");
 			return false;
-		}
-		else {
+		} else {
 			owner.decreaseMoney(this.price);
 			this.setOwner(owner);
 			owner.addProperty(this);
 			return true;
 		}
-		
+
 	}
 
 	public boolean repOK() {
@@ -71,19 +70,23 @@ public abstract class OwnableSquare extends Square {
 			return false;
 		return true;
 	}
-	
+
 	@Override
 	public void landOn(Player player) {
 		GameController.getInstance().setCurrentLocationBuyable(this.isOwned && player.getTotalMoney() >= this.price);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
-		return "OwnableSquare [price=" + price + ", owner=" + owner + ", isOwned=" + isOwned + ", name=" + name
-				+ ", description=" + description + "]";
+		String str = "OwnableSquare [price=" + price + ", owner=";
+		str += (owner == null) ? "null" : owner.getNickName();
+		str += ", isOwned=" + isOwned + ", name=" + name + ", description=" + description + "]";
+		return str;
 	}
-	
+
 }
