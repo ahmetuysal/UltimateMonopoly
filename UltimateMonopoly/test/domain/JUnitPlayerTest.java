c540c1af2e4052c227f79ab87123ddfa977401c9
Halil Eralp Koçaş <hkocas15@ku.edu.tr>
2018-12-16 18:28:59 +0300

    Overview and coverage fix
    Overviews are added for tested methods. Missing paths are covered.

--- UltimateMonopoly/test/domain/JUnitPlayerTest.java
+++ UltimateMonopoly/test/domain/JUnitPlayerTest.java
@@ -17,14 +17,14 @@ import domain.card.RollThreeCard;
  */
 
 public class JUnitPlayerTest {
-	
+
 	@BeforeEach
 	void refreshGameController() {
 		Token.initializeAvailableTokens();
 		GameController.getInstance().setPlayers(new ArrayList<>());
 	}
 
-	///////////////////////////Black-Box Test/////////////////////////////////
+	/////////////////////////// Black-Box Test/////////////////////////////////
 	@Test
 	void testGoToJail() {
 		GameController.getInstance().registerUser("Eralp", "Boot.png");
@@ -32,12 +32,11 @@ public class JUnitPlayerTest {
 		player.goToJail();
 		assertEquals(player.isInJail(), true);
 	}
-	
-	
+
 	//////////////////////////////////////////////////////////////////////////
-	
-	/////////////////////////Glass-Box Test///////////////////////////////////
-	
+
+	///////////////////////// Glass-Box Test///////////////////////////////////
+
 	@Test
 	void testIncreaseMoney() {
 		int money = 100;
@@ -47,45 +46,50 @@ public class JUnitPlayerTest {
 		assertEquals(startMoney + money, player.getTotalMoney());
 		assertTrue(player.repOK());
 	}
-	
+
 	@Test
 	void testDecreaseMoney() {
 		int money = 100;
 		Player player = new Player("Eralp");
 		int startMoney = player.getTotalMoney();
 
-		boolean isDecreased = player.decreaseMoney(startMoney+1);
+		boolean isDecreased = player.decreaseMoney(startMoney + 1);
 		assertEquals(isDecreased, false);
-		
+
 		player.decreaseMoney(money);
 		assertEquals(startMoney - money, player.getTotalMoney());
-		
+
 		assertTrue(player.repOK());
 	}
-	
+
 	@Test
 	void testGoToJailGlassBox() {
 		GameController.getInstance().registerUser("Eralp", "Boot.png");
 		Player player = GameController.getInstance().getPlayerList().get(0);
 		player.goToJail();
 		assertEquals(player.isInJail(), true);
-		assertEquals(player.getToken().getLocation(), GameController.getInstance().getBoard().getSquareLocationFromName("Jail"));
+		assertEquals(player.getToken().getLocation(),
+				GameController.getInstance().getBoard().getSquareLocationFromName("Jail"));
 		assertEquals(player.getJailTime(), 3);
-		
+
 	}
-	
+
 	@Test
 	void testAddCard() {
 		Player player = new Player("Eralp");
-		OwnableCard card = new RollThreeCard("","", 1, 2, 3);
+		OwnableCard card = new RollThreeCard("", "", 1, 2, 3);
 		int oldNum = player.getCards().size();
+
+		player.setCards(null);
+		assertEquals(player.getCards(), null);
 		player.addCard(card);
-		
+
+		assertNotEquals(player.getCards(), null);
 		assertEquals(oldNum + 1, player.getCards().size());
 		assertEquals(player.getCards().contains(card), true);
-		
+
 	}
-	
+
 	//////////////////////////////////////////////////////////////////////////
-	
+
 }
