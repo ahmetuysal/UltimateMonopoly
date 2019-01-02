5c5f4ec79ee5f367f3128db1ced37b502be5d38a
Ahmet Uysal <auysal16@ku.edu.tr>
2019-01-02 16:29:17 +0300

    Added a FIFO list to keep animations and multithreading to show on UI.

--- UltimateMonopoly/src/ui/UIToken.java
+++ UltimateMonopoly/src/ui/UIToken.java
@@ -2,8 +2,13 @@ package ui;
 
 import java.awt.Container;
 import java.awt.Image;
+import java.awt.Point;
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -17,26 +22,41 @@ import domain.util.PropertyListener;
 public class UIToken extends JLabel implements PropertyListener {
 
 	private Location location;
-	
+	private LinkedList<TokenMoveAnimation> waitingAnimations;
+
 	public UIToken(Token token, int tokenSize) {
 		super(getIconFromFileName(token.getTokenImageName(), tokenSize));
 		this.setSize(tokenSize, tokenSize);
 		this.setLocation(0, 0);
 		this.setVisible(true);
-		changeLocation(token.getLocation());
+		waitingAnimations = new LinkedList<>();
 		token.addPropertyListener("location", this);
+
+		final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+		scheduler.scheduleAtFixedRate(new Runnable() {
+			public void run() {
+				if (waitingAnimations != null && !waitingAnimations.isEmpty()) {
+					TokenMoveAnimation currentAnim = waitingAnimations.getFirst();
+					double ratio = currentAnim.increaseCompletedRate();
+					changeLocation(currentAnim.getStartingLocation(), currentAnim.getEndingLocation(), ratio);
+					if (ratio >= 1)
+						waitingAnimations.removeFirst();
+				}
+			}
+
+		}, 0, 50, TimeUnit.MILLISECONDS);
 	}
-	
+
 	private static ImageIcon getIconFromFileName(String fileName, int tokenSize) {
 		Image tmp = null;
 		try {
 			String osName = System.getProperty("os.name").toLowerCase();
-	        if(osName.contains("mac")){
-	        	tmp = ImageIO.read(new File("./images/tokens/" + fileName));
-	        } else {
+			if (osName.contains("mac")) {
+				tmp = ImageIO.read(new File("./images/tokens/" + fileName));
+			} else {
 				tmp = ImageIO.read(new File(".\\images\\tokens\\" + fileName));
-	        }
-			
+			}
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -44,20 +64,19 @@ public class UIToken extends JLabel implements PropertyListener {
 		tmp = tmp.getScaledInstance(tokenSize, tokenSize, Image.SCALE_SMOOTH);
 		return new ImageIcon(tmp);
 	}
-	
-	private void changeLocation(Location loc) {
-		Location oldLoc = this.location;
-		this.location = loc;
+
+	private void changeLocation(Location oldLoc, Location newLoc, double completedRatio) {
 		Container parent = getParent();
 		if (parent != null)
-			((GameRoomPanel)getParent()).TokenLocationChanged(this, oldLoc, location);
+			((GameRoomPanel) getParent()).TokenLocationChanged(this, oldLoc, newLoc, completedRatio);
 	}
-	
+
 	@Override
 	public void onPropertyEvent(PropertyEvent e) {
 		if (e.getPropertyName().equals("location")) {
-			changeLocation((Location) e.getNewValue());
-			// TODO start animation
+			this.location = (Location) e.getNewValue();
+			waitingAnimations.addLast(new TokenMoveAnimation((Location) e.getOldValue(), (Location) e.getNewValue()));
+			// changeLocation((Location) e.getNewValue());
 		}
 	}
 
