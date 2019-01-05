607477bcd72f03aa9bfff2fea85db1d6f49bf8c8
Ahmet Uysal <auysal16@ku.edu.tr>
2019-01-05 21:07:22 +0300

    Refactored UIToken under animatable interface.

--- UltimateMonopoly/src/ui/UIToken.java
+++ UltimateMonopoly/src/ui/UIToken.java
@@ -2,27 +2,26 @@ package ui;
 
 import java.awt.Container;
 import java.awt.Image;
-import java.awt.Point;
 import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
+
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
+import domain.Board;
 import domain.Token;
 import domain.square.Location;
 import domain.util.PropertyEvent;
 import domain.util.PropertyListener;
 
-public class UIToken extends JLabel implements PropertyListener {
+public class UIToken extends JLabel implements PropertyListener, Animatable {
 
 	private Location location;
-	private LinkedList<TokenMoveAnimation> waitingAnimations;
+	private LinkedList<Animation> waitingAnimations;
 
 	public UIToken(Token token, int tokenSize) {
 		super(getIconFromFileName(token.getTokenImageName(), tokenSize));
@@ -31,20 +30,6 @@ public class UIToken extends JLabel implements PropertyListener {
 		this.setVisible(true);
 		waitingAnimations = new LinkedList<>();
 		token.addPropertyListener("location", this);
-
-		final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-		scheduler.scheduleAtFixedRate(new Runnable() {
-			public void run() {
-				if (waitingAnimations != null && !waitingAnimations.isEmpty()) {
-					TokenMoveAnimation currentAnim = waitingAnimations.getFirst();
-					double ratio = currentAnim.increaseCompletedRate();
-					changeLocation(currentAnim.getStartingLocation(), currentAnim.getEndingLocation(), ratio);
-					if (ratio >= 1)
-						waitingAnimations.removeFirst();
-				}
-			}
-
-		}, 0, 50, TimeUnit.MILLISECONDS);
 	}
 
 	private static ImageIcon getIconFromFileName(String fileName, int tokenSize) {
@@ -65,7 +50,7 @@ public class UIToken extends JLabel implements PropertyListener {
 		return new ImageIcon(tmp);
 	}
 
-	private void changeLocation(Location oldLoc, Location newLoc, double completedRatio) {
+	public void changeLocation(Location oldLoc, Location newLoc, double completedRatio) {
 		Container parent = getParent();
 		if (parent != null)
 			((GameRoomPanel) getParent()).TokenLocationChanged(this, oldLoc, newLoc, completedRatio);
@@ -75,9 +60,17 @@ public class UIToken extends JLabel implements PropertyListener {
 	public void onPropertyEvent(PropertyEvent e) {
 		if (e.getPropertyName().equals("location")) {
 			this.location = (Location) e.getNewValue();
-			waitingAnimations.addLast(new TokenMoveAnimation((Location) e.getOldValue(), (Location) e.getNewValue()));
+			if (Board.isCornerSquare((Location) e.getNewValue()))
+				waitingAnimations.addLast(new TokenMoveAnimation(this, (Location) e.getOldValue(), (Location) e.getNewValue(), 0.05));
+			else
+				waitingAnimations.addLast(new TokenMoveAnimation(this, (Location) e.getOldValue(), (Location) e.getNewValue()));
 			// changeLocation((Location) e.getNewValue());
 		}
+	}
+
+	@Override
+	public LinkedList<Animation> getWaitingAnimations() {
+		return waitingAnimations;
 	}
 
 }
