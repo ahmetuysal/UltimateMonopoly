d3b7d58ba48d83327617cee82c626362c6530efc
Merve <mkarakas16@ku.edu.tr>
2019-01-05 15:04:46 +0300

    Removed unused methods from PlayerPanel

--- UltimateMonopoly/src/ui/PlayerPanel.java
+++ UltimateMonopoly/src/ui/PlayerPanel.java
@@ -32,49 +32,13 @@ public class PlayerPanel extends JPanel {
 		
 		
 		setSize(width, height);
-		setLayout(null);
-		
+		setLayout(null);		
 		setVisible(true);
-		
-//		initLabels();
-//		addTabbedPane();
+
 		initPlayerInfos();
 		
 	}
 	
-	private void initLabels(){
-		int x = panelWidth / 20;
-		int y = 0;
-		int width = panelWidth - 2*x;
-		List<Player> players = controller.getPlayerList();
-		int height = players.isEmpty()? panelHeight : panelHeight / players.size();
-		
-		if(height > panelHeight / 10){
-			height = panelHeight / 20;
-		}
-		
-		for (int i=0; i<players.size(); i++){
-			
-			Player player = players.get(i);
-			
-			
-			ObserverLabel moneyLabel = new ObserverLabel(width, height, player.getNickName(), "playerInfo");
-			
-			moneyLabel.setBounds(x, y + i*height , width, height);
-			moneyLabel.setSize(width, height);
-			moneyLabel.setVisible(true);
-			
-			
-			moneyLabel.setFont(new Font("Sans", Font.CENTER_BASELINE, height/2));
-			
-			
-			player.addPropertyListener("money", moneyLabel);
-			
-			add(moneyLabel);
-		}
-		
-		
-	}
 	
 	private void initPlayerInfos() {
 		playerInfoPane = new JTabbedPane();
@@ -96,44 +60,6 @@ public class PlayerPanel extends JPanel {
 		
 		this.add(playerInfoPane);
 	}
-	
-	
-	
-	private void addTabbedPane() {
-		playerInfoPane = new JTabbedPane();
-		
-		JComponent money = makeTextPanel("Money");
-		money.setBackground(Color.white);
-		playerInfoPane.addTab("Money", null, money, "Does nothing");
-		
-		JComponent properties = makeTextPanel("Properties");
-		playerInfoPane.addTab("Properties", null, properties, "Does Nothing");
-		
-		JTabbedPane tabbedpane = new JTabbedPane();
-		tabbedpane.setVisible(true);
-		tabbedpane.setBackground(this.getBackground());
-		tabbedpane.addTab("name",null,(JComponent) makeTextPanel("Properties"), "Does nothing");
-//		tabbedpane.addTab("money", null, money, "noo");
-		
-		
-		playerInfoPane.addTab("Try1",null,tabbedpane,"Does nothing");
-		playerInfoPane.setVisible(true);
-		playerInfoPane.setBackground(this.getBackground());
-		playerInfoPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
-		playerInfoPane.setBounds(this.getWidth()/8, this.getHeight()/8, 3*this.getWidth()/4, 3*this.getHeight()/4);
-		
-		this.add(playerInfoPane);
-	}
-	
-	protected JComponent makeTextPanel(String text) {
-        JPanel panel = new JPanel(false);
-        JLabel filler = new JLabel(text);
-        filler.setHorizontalAlignment(JLabel.CENTER);
-        panel.setLayout(new GridLayout(1, 1));
-        panel.add(filler);
-        return panel;
-    }
-	
 	
 	
 	private JTabbedPane createPlayerTabbedPane(Player player) {
@@ -167,24 +93,6 @@ public class PlayerPanel extends JPanel {
 		return pane;
 	}
 	
-	private JTabbedPane createPropertyTabbedPane(Player player) {
-		JTabbedPane pane = new JTabbedPane();
-		
-		ObserverLabel properties = new ObserverLabel(40,100, player.getNickName(), "properties");
-		properties.setHorizontalAlignment(JLabel.LEFT);
-		properties.setVerticalAlignment(JLabel.TOP);
-		
-		JPanel panel2 = new JPanel(false);
-		panel2.setLayout(new GridLayout(1,1));
-		panel2.add(properties);
-		
-		pane.addTab("Properties", panel2);
-		
-		pane.setVisible(true);
-		pane.setBackground(this.getBackground());
-		
-		return pane;
-	}
-	
+
 	
 }
