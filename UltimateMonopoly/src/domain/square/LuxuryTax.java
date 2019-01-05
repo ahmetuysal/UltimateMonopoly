ca9942bce71fe1c226e0209cfc280786dd381c7e
Halil Eralp Koçaş <hkocas15@ku.edu.tr>
2019-01-05 21:01:44 +0300

    LuxuryTax and SqueezePlay
    LuxuryTax and SquuezePlay are implemented.

--- UltimateMonopoly/src/domain/square/LuxuryTax.java
+++ UltimateMonopoly/src/domain/square/LuxuryTax.java
@@ -1,5 +1,6 @@
 package domain.square;
 
+import domain.GameController;
 import domain.Player;
 
 public class LuxuryTax extends Square {
@@ -13,6 +14,9 @@ public class LuxuryTax extends Square {
 	public void landOn(Player player) {
 		super.landOn(player);
 		// TODO implement square specific logic
+		int payment = 75;
+		player.decreaseMoney(payment);
+		GameController.getInstance().increasePoolMoney(payment);
 	}
 
 }
