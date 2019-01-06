8a37ef8ef30d9457e5b294c8be0d5bb25946b9bb
Halil Eralp Koçaş <hkocas15@ku.edu.tr>
2019-01-07 01:59:40 +0300

    Bug fix for owner
    Anyone except the owner of that property cannot activate the building buttons.

--- UltimateMonopoly/src/domain/GameController.java
+++ UltimateMonopoly/src/domain/GameController.java
@@ -221,6 +221,9 @@ public class GameController extends Observable {
 			setCurrentPlayer(currentPlayerIndex);
 			actionQueue.clear();
 			publishPropertyEvent("isTurnFinished", false, true);
+			publishPropertyEvent("buyHouse", true, false);
+			publishPropertyEvent("buyHotel", true, false);
+			publishPropertyEvent("buySkyscraper", true, false);
 		}
 	}
 
