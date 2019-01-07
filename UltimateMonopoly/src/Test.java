d62784c580bbf2d2e0f2358f9b2f59cd0d3ed33b
Meltem Gunay <mgunay15@ku.edu.tr>
2019-01-07 18:50:03 +0300

    Communication facade is removed from Test

--- UltimateMonopoly/src/Test.java
+++ UltimateMonopoly/src/Test.java
@@ -4,9 +4,8 @@ import ui.MonopolyFrame;
 public class Test {
 
 	public static void main(String[] args) {
-	//	GameController.getInstance();
 		new MonopolyFrame();
-		//new CommunicationFacade();
+		
 	}
 
 }
